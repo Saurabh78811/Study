238fd5ea3ae415999b9e9ae6e2c4f62ea5c110b7
Saurabh Verma <87320652+Saurabh78811@users.noreply.github.com>
2026-04-18 09:14:10 +0000

    aa

---
+++ base/dataType.js
@@ -0,0 +1,14 @@
+let name = "saurabh verma"
+let age = "18"
+let isLoggedIn = true
+let state
+
+/*
+1. Number => 2 to power of 53 
+2. bigint = > large number to use this
+3. boolean => true/false
+4. null => standalone value
+5. undefined 
+6. symbol => unique
+7. object
+*/
---
+++ base/variable.js
@@ -0,0 +1,25 @@
+/*
+What is veriable ?
+>a variable is a named container used to store and manage data in memory
+*/
+const accountId = 144556 // const Value not change
+let accountEmail = "vsaurabh78811@gmail.com" 
+var accountPassword = "123456"
+accountCity = "Noida"
+
+accountEmail = "test@gmail.com"
+accountPassword = "123123"
+accountCity = "Ballia"
+
+
+
+
+/*
+Please not to use Var
+because of issue in block scope and functional scope 
+*/
+
+console.log(accountEmail);
+console.table([accountEmail,accountPassword,accountCity])
+
+
